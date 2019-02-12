93458fe75d566dc002ad2711f4ef37be70aafa21
vmgabriel <vmgabriel96@gmail.com>
2019-02-12 10:34:27 -0500

    Agregadas barras de tiempo

--- js/grafico.js
+++ js/grafico.js
@@ -1,17 +1,37 @@
-var tam_base = 300;
-var inicio_nombres = 10;
-var inicio_barras = 70;
+var procesos = 13; //Cantidad de Procesos en el sistema
+var onOff = true; // Ejecucion de Pausa/play
+var dispatch = false; // Evalua si algo cambio en la lista de procesos
+var proceso; // Hilo base
+
+var tiempoGlobalEjecucion = 0; //Tiempo de ejecucion de proceso
+
+// Variables de grafica por defecto
+var inicioNombres = 10;
+var inicioBarras = 80;
+var distanciaEntreProcesos = 20;
+var margenProceso = 40;
+var anchoBarra = 10;
+var distanciaTiempoTexto = 40;
+var distanciaEntreBarras = 60;
 
 var myCanvas = document.getElementById("myCanvas");
-myCanvas.width = tam_base;
-myCanvas.height = tam_base;
+var myTimeExec = document.getElementById("timeExec");
+var myInitTime = document.getElementById("inicioTiempo");
+var myCantProcess = document.getElementById("cantidadProcesos");
+
+myCanvas.width = 120;
+myCanvas.height = 300;
 
 var ctx = myCanvas.getContext("2d");
+
+// Metodos Base
 
 function tiempo() {
     var d = new Date();
     return d.toLocaleTimeString();
 }
+
+// Dibujo Base
 
 function dibujarBase(ctx) {
     ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
@@ -41,11 +61,65 @@ function dibujarTexto(ctx, txt, x, y, stroke_color) {
     ctx.fillText(txt,x,y);
 }
 
-function arrancarEjecucion() {
-    myCanvas.width += 10;
-    dibujarBase(ctx);
-    dibujarBarra(ctx, inicio_barras+10, 10, 50, 10, '#DF0101');
-    dibujarTexto(ctx, tiempo(),  3, 20, "#08298A");
+// Dibujo mas especifico
+
+function lineasTiempo(ctx) {
+    var grosorBarra = 2;
+    var colorBarra = "#08088A";
+    var inicioPintadoBarra = 30;
+    var largoBarras = 2000;
+
+    var posicionTexto = 20;
+
+    if (procesos > 11 && dispatch == false) {
+        myCanvas.height += (distanciaTiempoTexto*(procesos-11));
+        dispatch = true;
+    }
+
+    var i=0;
+    while (i <= tiempoGlobalEjecucion) {
+        dibujarBarra(ctx, inicioBarras+(distanciaEntreBarras*i), inicioPintadoBarra, grosorBarra, largoBarras, colorBarra);
+        dibujarTexto(ctx, i, inicioBarras+(distanciaEntreBarras*i), posicionTexto, colorBarra);
+        i += 1;
+    }
 }
 
-var myVar = setInterval(arrancarEjecucion, 1000);
+// Manejo de Proceso Principal
+
+function arrancarEjecucion() {
+    myTimeExec.innerHTML = tiempoGlobalEjecucion;
+    myCanvas.width += distanciaEntreBarras;
+
+    lineasTiempo(ctx);
+
+    var temp = 0;
+    while (temp < procesos) {
+        dibujarTexto(ctx, "Base"+temp,  inicioNombres, margenProceso+((temp+1)*distanciaEntreProcesos), "#08298A");
+        dibujarBarra(ctx, inicioBarras, (margenProceso + 10)+(temp*distanciaEntreProcesos), 40, anchoBarra, '#DF0101');
+        temp += 1;
+    }
+    tiempoGlobalEjecucion += 1;
+}
+
+function pausar() {
+    if (!onOff) {
+        // Pausar
+        onOff = true;
+        clearInterval(proceso);
+        myInitTime.innerHTML = "Reanudar Ejecucion";
+    } else {
+        // Ejecutar
+        onOff = false;
+        proceso = setInterval(arrancarEjecucion, 1000);
+        myInitTime.innerHTML = "Pausar Ejecucion";
+    }
+}
+
+function detener() {
+    onOff = true;
+    clearInterval(proceso);
+    tiempoGlobalEjecucion = 0;
+    myInitTime.innerHTML = "Iniciar Ejecucion";
+    myTimeExec.innerHTML = 0;
+    dibujarBase(ctx);
+}
